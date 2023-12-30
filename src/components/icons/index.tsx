c59999ea27fadd8515045e0e053c3b6ebef0f0f6
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-12-29 21:45:26 -0300

    feat: add icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,10 @@
+import { IconLogo as Logo } from "./icon-logo";
+import { IconSearch as Search } from "./icon-search";
+
+import { Upload } from "lucide-react";
+
+export const Icon = {
+  Logo,
+  Search,
+  Upload
+};
