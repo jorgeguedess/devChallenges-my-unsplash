84b864fc740d4a62623c79c6450fe910da2ee96c
Jorge Guedes <jorge_guedes00@hotmail.com>
2024-01-02 23:54:34 -0300

    feat: added new upload method by URL or File

--- src/components/upload-photo.tsx
+++ src/components/upload-photo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import useStorage from "@/hooks/useStorage";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
 
 import { Button } from "@/components/ui/button"
 import { DialogClose, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
@@ -11,21 +11,37 @@ import { usePhoto } from "@/context/contextImage";
 
 export const UploadPhoto = () => {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
-    const { startUpload } = useStorage("images");
+    const [selectedURL, setSelectedURL] = useState<string | null>(null);
+    const { startUploadFile, startUploadUrl } = useStorage("images");
     const {legend, setLegend} = usePhoto();
+    const [typeUpload, setTypeUpload] = useState<"url" | "file">("file");
 
+    const handleTypeUpload = (event: React.MouseEvent<HTMLButtonElement>) => {
+      const name = (event.target as HTMLButtonElement).name as any;
+      if (name === typeUpload) return;
+      console.log('mudou')
+      setTypeUpload(name);
+      setSelectedFile(null);
+      setSelectedURL(null);
+    };
+    
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
           setSelectedFile(event.target.files[0]);
         }
       };
 
+
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if (selectedURL) {
+          startUploadUrl(selectedURL);
+        }
         if (selectedFile) {
-          startUpload(selectedFile);
+          startUploadFile(selectedFile);
         }
         setSelectedFile(null);
+        setSelectedURL(null);
         setLegend("");
       };
 
@@ -52,24 +68,56 @@ export const UploadPhoto = () => {
       </div>
       <div className="flex flex-col gap-2 items-start">
         <Label htmlFor="url">
-          Photo URL
+          Upload by
         </Label>
-        <Input
-        onChange={handleFileChange}
-          placeholder="https://images.unsplash.com/photo-1584395630827-860eee694d7b?ixlib=r.."
-          className="block w-full text-sm text-gray-500
-          file:me-4 file:py-2 file:px-4
-          file:rounded-lg file:border-0
-          file:text-sm file:font-semibold
-          file:bg-blue-600 file:text-white
-          hover:file:bg-blue-700
-          file:disabled:opacity-50 file:disabled:pointer-events-none
-          dark:file:bg-blue-500
-          dark:hover:file:bg-blue-400 border border-primary cursor-pointer"
-          type="file"
-          id="url"
-          required
-        />
+        <div className="flex flex-wrap items-center justify-between w-full gap-2">
+          <div className="flex w-full bg-gray-200 rounded-lg p-1 flex-1">
+            <Button 
+            type="button" 
+            variant="link" 
+            className={`text-cyan-950 flex-1 ${typeUpload === "url" && "bg-white"}`} 
+            onClick={handleTypeUpload}
+            name="url"
+            >
+              URL
+            </Button>
+            <Button 
+            type="button" 
+            variant="link" 
+            className={`text-cyan-950 flex-1 ${typeUpload === "file" && "bg-white"}`} 
+            onClick={handleTypeUpload}
+            name="file"
+            >
+              File
+          </Button>
+          </div>
+          {typeUpload === "file" ?  
+            <Input
+              onChange={handleFileChange}
+                placeholder="https://images.unsplash.com/photo-1584395630827-860eee694d7b?ixlib=r.."
+                className="flex-1 block w-full text-sm text-gray-500
+                file:me-4 file:py-2 file:px-4
+                file:rounded-lg file:border-0
+                file:text-sm file:font-semibold
+                file:bg-blue-600 file:text-white
+                hover:file:bg-blue-700
+                file:disabled:opacity-50 file:disabled:pointer-events-none
+                dark:file:bg-blue-500
+                dark:hover:file:bg-blue-400 border border-primary cursor-pointer min-w-16"
+                type="file"
+                id="url"
+                required
+              /> : 
+              <Input
+              onChange={({target}) => setSelectedURL(target.value)}
+              className="flex-1 block w-full"
+              placeholder="Add a link"
+              type="url"
+              required
+              />
+            }
+       
+        </div>
       </div>
     </div>
     <DialogFooter>
@@ -77,7 +125,10 @@ export const UploadPhoto = () => {
         <Button type="button" variant="ghost" className="text-input">Cancel</Button>
       </DialogClose>
       <DialogClose asChild>
-      <Button type="submit" disabled={!selectedFile} className="flex items-center">
+      <Button 
+      type="submit" 
+      disabled={!selectedFile && !selectedURL}
+      className="flex items-center">
         Submit
       </Button>
       </DialogClose>
--- src/hooks/useStorage.tsx
+++ src/hooks/useStorage.tsx
@@ -4,72 +4,96 @@ import { v4 as uuidv4 } from "uuid";
 import { db, storage } from "@/services/firebase";
 import { usePhoto } from "@/context/contextImage";
 
-import { deleteObject, getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { addDoc, collection, doc, deleteDoc } from "firebase/firestore";
 
 import { ImageDataProps } from "@/types/image";
 
-
 const useStorage = (collectionName: string) => {
   const [progress, setProgress] = useState<number>(0);
   const [error, setError] = useState<Error | null>(null);
-  const {legend} = usePhoto();
+  const { legend } = usePhoto();
 
-  const startUpload = async (file: File) => {
+  const convertUrlToFile = async (url: string) => {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const filename = url.substring(url.lastIndexOf("/") + 1);
+    const fileType = blob.type;
+    return new File([blob], filename, { type: fileType });
+  };
+
+  const startUpload = async (file: File | string) => {
+    try {
+      const isUrl = typeof file === "string";
+      const fileId = uuidv4();
+      const formatFile = isUrl ? "jpg" : file.type.split("/")[1];
+
+      const storageRef = ref(storage, `${collectionName}/${fileId}.${formatFile}`);
+      const uploadTask = uploadBytesResumable(storageRef, isUrl ? await convertUrlToFile(file as string) : file as File);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(progress);
+        },
+        (error) => {
+          setError(error);
+        },
+        async () => {
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+          setProgress(progress);
+
+          // store data into firestore
+          await addDoc(collection(db, collectionName), {
+            id: fileId,
+            url: downloadURL,
+            createdAt: new Date(),
+            name: legend,
+          });
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const startUploadFile = async (file: File) => {
     if (!file) return;
+    startUpload(file);
+  };
 
-    const fileId = uuidv4();
-    const formatFile = file.type.split("/")[1];
-
-    const storageRef = ref(storage, `${collectionName}/${fileId}.${formatFile}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progress);
-      },
-      (error) => {
-        setError(error);
-      },
-      async () => {
-        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-        setProgress(progress);
-
-        // store data into firestore
-        await addDoc(collection(db, collectionName), {
-          id: fileId,
-          url: downloadURL,
-          createdAt: new Date(),
-          name: legend,
-        });
-      }
-    );
+  const startUploadUrl = async (url: string) => {
+    if (!url) return;
+    startUpload(url);
   };
 
   const deleteImage = async (image: ImageDataProps) => {
     try {
-        const deleteRef = ref(storage,image.url)
-        await deleteObject(deleteRef);
+      const deleteRef = ref(storage, image.url);
+      await deleteObject(deleteRef);
 
-        const docRef = doc(db, collectionName, image.id)
-        await deleteDoc(docRef);
+      const docRef = doc(db, collectionName, image.id);
+      await deleteDoc(docRef);
 
-        return {succes: "image delete success"}
+      return { success: "image delete success" };
     } catch (error) {
-        console.error(error);
-        // setError(error?.message);
-        return {error: "image delete fail"}
+      console.error(error);
+      return { error: "image delete fail" };
     }
-  }
-  
+  };
 
   return {
     progress,
     error,
-    startUpload,
+    startUploadFile,
+    startUploadUrl,
     deleteImage,
   };
 };
